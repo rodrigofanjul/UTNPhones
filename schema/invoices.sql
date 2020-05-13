75100d702d7299696ed36f7676f741a9deebff58
Rodrigo Fanjul <rodrii.fan@gmail.com>
2020-05-12 22:21:32 -0300

    Updated schema invoices.sql

--- schema/invoices.sql
+++ schema/invoices.sql
@@ -1,9 +1,10 @@
+DROP PROCEDURE IF EXISTS sp_invoices;
 DELIMITER //
 CREATE PROCEDURE sp_invoices()
 BEGIN
-    DECLARE i INT DEFAULT -1;
+    DECLARE i BIGINT DEFAULT -1;
 	DECLARE call_id INT;
-	DECLARE phoneline_id INT;
+	DECLARE phoneline_id BIGINT;
     DECLARE calls_totalprice FLOAT DEFAULT 0;
     DECLARE calls_quantity INT DEFAULT 0;
     DECLARE cost_price FLOAT DEFAULT 4.50;
@@ -12,7 +13,7 @@ BEGIN
     DECLARE new_invoice_id INT;
 	DECLARE cur_calls_finished INT DEFAULT 0;
 	
-    DECLARE cur_calls CURSOR FOR SELECT id, id_line_origin FROM calls WHERE id_invoice IS NULL GROUP BY id;
+    DECLARE cur_calls CURSOR FOR SELECT id, id_line_origin FROM calls WHERE id_invoice IS NULL GROUP BY id_line_origin;
     DECLARE CONTINUE handler FOR NOT FOUND SET cur_calls_finished = 1;
 	
     START TRANSACTION; -- Inicio de la transaccion
@@ -28,7 +29,7 @@ BEGIN
 				IF(cur_calls_finished = 1) THEN leave loop_calls; END IF;
 				
 				IF(i <> phoneline_id) THEN
-					SELECT (SUM(call_totalprice) * cost_price), COUNT(*) INTO calls_totalprice, calls_quantity FROM calls WHERE id_invoice IS NULL AND id_line_origin = phoneline_id;
+					SELECT SUM(call_totalprice), COUNT(*) INTO calls_totalprice, calls_quantity FROM calls WHERE id_invoice IS NULL AND id_line_origin = phoneline_id;
 
 					INSERT INTO invoices(id_phoneline, invoice_calls_quantity, invoice_cost_price, invoice_totalprice, invoice_date, invoice_is_paid, invoice_expiration_date)
 					VALUES(phoneline_id, calls_quantity, cost_price, calls_totalprice, issue_date, FALSE, expiration_date);
@@ -37,7 +38,7 @@ BEGIN
 					SET i = phoneline_id;
 				END IF;
 				
-				UPDATE calls SET id_invoice = new_invoice_id WHERE id_call = call_id;
+				UPDATE calls SET id_invoice = new_invoice_id WHERE id = call_id;
 				
 			END LOOP loop_calls;
 		
@@ -49,6 +50,7 @@ DELIMITER ;
 
 -- SHOW PROCESSLIST;
 -- SET GLOBAL event_scheduler = ON;
+DROP EVENT IF EXISTS event_invoices;
 DELIMITER //
 CREATE EVENT event_invoices ON SCHEDULE EVERY "1" MONTH	STARTS "2020-06-01" DO
 BEGIN
